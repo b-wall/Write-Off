dc67d3f9c1bf865d36810b4e3abd9393fa8806b8
Ben W <sargeantscigar@gmail.com>
2022-07-03 12:50:34 +0800

    Add Login and Register Functionality - Style Login and Register page - Handle requests in backend to: -- create new users -- check if username is available -- log in and authenticate existing users

--- writeoff/urls.py
+++ writeoff/urls.py
@@ -5,9 +5,11 @@ urlpatterns = [
     path("", views.index, name='index'),
     path("hello/", views.landingpage, name='landing'),
     path("register/", views.register, name='register'),
-    path("login/", views.login, name='login'),
+    path("login/", views.login_view, name='login'),
+    path("logout/", views.logout_view, name='logout'),
     path("characters/", views.characters, name='characters'),
     path("timeline/", views.timeline, name='timeline'),
     path("write/", views.write, name='write'),
     path("practice/", views.practice, name='practice'),
+    path("register/", views.register, name='register'),
 ]
--- writeoff/views.py
+++ writeoff/views.py
@@ -1,7 +1,12 @@
+from django.db import IntegrityError
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 from .forms import BookForm
+from .models import User
 
 
 # @login_required
@@ -14,20 +19,28 @@ def landingpage(request):
     """Display landing page"""
     return render(request, "landing.html")
 
+# @login_required
+
 
 def characters(request):
     """Display Character Page"""
     return render(request, "characters.html")
+
+# @login_required
 
 
 def timeline(request):
     """Display Timeline View"""
     return render(request, "timeline.html")
 
+# @login_required
+
 
 def write(request):
     """Display page to write content"""
     return render(request, "write.html", {'form': BookForm()})
+
+# @login_required
 
 
 def practice(request):
@@ -37,9 +50,52 @@ def practice(request):
 
 def register(request):
     """Allow user to register an account"""
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "register.html", {
+                "message": "Error: passwords must match"
+            })
+
+        # Create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "register.html", {
+                "message": "Username unavailable"
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+
     return render(request, "register.html")
 
 
-def login(request):
+def login_view(request):
     """Display login page"""
+    if request.method == "POST":
+
+        # Log user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render("login.html", {
+                "message": "Invalid username/password"
+            })
     return render(request, "login.html")
+
+
+def logout_view(request):
+    """Logout User"""
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
